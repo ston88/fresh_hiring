5cca6acc85b6488a12c50050eacb6de9b0d58a68
Rhys Davis <rhys.l.davis@gmail.com>
2019-06-07 18:39:12 +1000

    Fixing email path

--- lib/fresh_hiring_web/views/email_view.ex
+++ lib/fresh_hiring_web/views/email_view.ex
@@ -2,7 +2,9 @@ defmodule FreshHiringWeb.EmailView do
   use FreshHiringWeb, :view
   alias FreshHiringWeb.Router.Helpers, as: Routes
 
+  require Logger
   def sign_in_url(page, token) do
+    Logger.info "Verify Path: #{inspect Routes.session_path(FreshHiringWeb.Endpoint, page, token: token)}"
     Routes.session_path(FreshHiringWeb.Endpoint, page, token: token)
   end
 end
