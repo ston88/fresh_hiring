00a6bfdfc541302ff349468e2c08edbc74d32bed
Rhys Davis <rhys.l.davis@gmail.com>
2019-06-07 18:06:05 +1000

    Fixing emails

--- lib/fresh_hiring_web/views/email_view.ex
+++ lib/fresh_hiring_web/views/email_view.ex
@@ -2,7 +2,7 @@ defmodule FreshHiringWeb.EmailView do
   use FreshHiringWeb, :view
   alias FreshHiringWeb.Router.Helpers, as: Routes
 
-  def sign_in_url(Endpoint, page, token) do
-    Routes.session_url(Endpoint, page, token)
+  def sign_in_url(page, token) do
+    Routes.session_url(FreshHiringWeb.Endpoint, page, token)
   end
 end
